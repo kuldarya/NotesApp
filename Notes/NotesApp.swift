ebe06febc33fa118d6b3cc250c58d499497b2566
Darya Kuliashova <kuldiand@gmail.com>
2023-04-10 16:12:15 +0200

    Create project

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Darya Kuliashova on 10/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
